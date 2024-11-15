a712a335ec2e68d173b4ff672aeca14de8320dc0
Vadosmik <liontv05@gmail.com>
2024-11-15 20:24:53 +0100

    maybe

--- static/client.js
+++ static/client.js
@@ -1,6 +1,6 @@
 const socket = io();
 
-socket.on ("new", function (data) {
+socket.on("new", function (data) {
     console.log(data);
     socket.emit(`hello server", "Игрок c id: $(data) подключился` );
 });
